3f3493c71760e6ffcef61c36a0cf540df2104628
Devang Dayal <devang@github.com>
2021-03-02 21:05:40 +0530

    Update videofacedetect.py

--- videofacedetect.py
+++ videofacedetect.py
@@ -19,7 +19,7 @@ obama_image = face_recognition.load_image_file("Devang.jpg")
 obama_face_encoding = face_recognition.face_encodings(obama_image)[0]
 
 # Load a second sample picture and learn how to recognize it.
-biden_image = face_recognition.load_image_file("Anshika.jpg")
+biden_image = face_recognition.load_image_file("AaronEckhart.jpg")
 biden_face_encoding = face_recognition.face_encodings(biden_image)[0]
 
 # Create arrays of known face encodings and their names-
@@ -29,7 +29,7 @@ known_face_encodings = [
 ]
 known_face_names = [
     "Devang Dayal",
-    "Anshika Verma"
+    "Aaron Eckhart"
 ]
 
 # Initialize some variables
